5e4f32e0a064bad41ff02dd51c49c8036956c3f6
junninho <junninhothomas1998@gmail.com>
2020-02-20 01:24:05 -0500

    final changes including comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,80 +1,107 @@
+// import necessary java libraries
 import java.io.*;
 import java.util.*;
 
+//import CSVReader and dependencies
 import com.opencsv.CSVReader;
 import java.nio.charset.StandardCharsets;
 
+// Artist node for linked list
 class Artist {
   String name;
   Artist next;
 
+  // Artist constructor
   public Artist(String artist) {
     name = artist;
   }
 }
 
+// Top Streaming Artists object
 class TopStreamingArtists {
-  private Artist first;
+  private Artist first; // initialize first
 
+  // constructor
   public TopStreamingArtists(){
     first = null;
   }
 
+  // isEmpty method to check if list is empty
   public boolean isEmpty(){
    return (first == null);
   }
 
+  // displays list in default order as read from file
   public void displayList() {
-    Artist current = first;
-    while(current != null) {
-      System.out.println(current.name);
-      current = current.next;
+    Artist current = first; // set current to first
+
+    // traverse through list
+    while(current != null) { // check if current is not null
+      System.out.println(current.name); // print name
+      current = current.next; // proceed to next item
     }
   }
 
+  // displays list in aphabetical order
   public void displayAlphabeticalList() {
-    Artist current = first;
+    Artist current = first; // set current to first
+
+    // array list to store items
     List<String> l = new ArrayList<>();
+
+    // traverse through linked list and add to array list for sort
     while(current != null) {
       l.add(current.name);
       current = current.next;
     }
 
+    // sort alphabetically, ingoring case sensitivity
     Collections.sort(l, String.CASE_INSENSITIVE_ORDER);
 
+    // print list
     for (String item : l) {
       System.out.println(item);
     }
     
   }
 
+  // insert method to add new artist to list
   public void insert(String artist) {
-    Artist newNode = new Artist(artist);
-    newNode.next = first;
-    first = newNode;
+    Artist newNode = new Artist(artist); // create new node
+    newNode.next = first; // assist value of first to node's next
+    first = newNode; // assign node to first
   }
 }
 
 public class Main {
 
   public static String[][] Array2DSolution(String file) throws Exception {
-    String[][] artistCount;
+    String[][] artistCount; // initialize 2D array
 
+    // read in file (create CSVReader)
     try (FileInputStream fis = new FileInputStream(file);
-      InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
-      CSVReader reader = new CSVReader(isr)) {
-      String[] nextLine;
-      List<String> artists = new ArrayList<>();
+    InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+    CSVReader reader = new CSVReader(isr)) {
+      String[] nextLine; // store line for processing
+      List<String> artists = new ArrayList<>(); // arrayList to store artists
 
+      // ignore first two lines
+      reader.readNext();
+      reader.readNext();
+
+      // extract artists from file and add to list
       while ((nextLine = reader.readNext()) != null) {
         artists.add(nextLine[2]);
       }
 
+      // initialize artistCount
       artistCount = new String[artists.size()][2];
 
-      int count = 0;
+      // count number of times artist appears
       for (String artist : artists) {
-          boolean exists = false;
+          boolean exists = false; // track if artist already exists in list
+
+          // check if artist appears and set exists flag to true
           for (int j = 0; j < artistCount.length; j++){
             try {
               if (artistCount[j][0].equals(artist)) {
@@ -84,6 +111,8 @@ public class Main {
               ;
             }
           }
+
+          // add artist to list if does not exist
           if (!exists) {
             for (int r = 0; r < artistCount.length ; r++ ) {
               if (artistCount[r][0] == null) {
@@ -92,6 +121,7 @@ public class Main {
                 break;
               }
             }
+            // if artist does exist, increment count
           } else {
             for (int r = 0; r < artistCount.length; r++ ) {
               try {
@@ -108,26 +138,34 @@ public class Main {
       }
     }
 
-    return artistCount;
+    return artistCount; // return 2D array
   }
+
+  // main method
   public static void main(String [] args) throws Exception{    
-    String[][] aList = Array2DSolution("data/regional-us-weekly-latest.csv");
+    String[][] aList = Array2DSolution("data/regional-us-weekly-latest.csv"); // create 2D array of artists and number of times in list
+    
+    // print how many times they appear
     for (int i = 0; i < aList.length; i++){
       if (aList[i][0] != null) {
         System.out.printf("%s appeared %s times\n", aList[i][0], aList[i][1]);
       }
     }
 
+    // spacers
     System.out.println("\n--\n");
 
-    TopStreamingArtists tsa = new TopStreamingArtists();
+
+    TopStreamingArtists tsa = new TopStreamingArtists(); // create new top streaming artists object
+
+    // add each artist in 2D array to linked list
     for (int r = 0; r < aList.length ; r++ ) {
       if (aList[r][0] != null) {
         tsa.insert(aList[r][0]);
       }
     }
 
-    tsa.displayAlphabeticalList();
+    tsa.displayAlphabeticalList(); // print alphabetical list
 
   }
 }
